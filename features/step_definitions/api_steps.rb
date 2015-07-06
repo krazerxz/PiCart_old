60578a3921ff54d9ca55cb499e70923ff14ac2e2
Chris Pomfret <chris.pomfret@bt.com>
2015-07-06 10:27:36 +0100

    Install rubocop. Use prejudice

--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -1,9 +1,7 @@
-When "I post a barcode to the api" do
+When 'I post a barcode to the api' do
   visit 'api/barcodes/new?barcode=12345678'
-
 end
 
-Then "a barcode is saved in the database" do
+Then 'a barcode is saved in the database' do
   expect(true).to be false
-
 end
