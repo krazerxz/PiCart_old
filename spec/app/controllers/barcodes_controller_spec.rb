e57f73c6a68a07c6caf2f06648bf2fdf7ed42b7d
Chris Pomfret <chris.pomfret@bt.com>
2015-07-08 11:48:13 +0100

    remove barcode controller spec that shouldnt be there

--- spec/app/controllers/barcodes_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe Api::BarcodesController, type: :controller do
-  xdescribe 'PUT #' do
-    it 'should do stuff' do
-      expect(true).to be false
-    end
-  end
-end
