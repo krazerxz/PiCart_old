673760b8ec628d4a240e7e0dccb7a8bd06913e29
Chris Pomfret <chris.pomfret@bt.com>
2015-07-08 09:24:29 +0100

    Products controller checks for local product before resolving barcode

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,7 +4,8 @@ class Api::ProductsController < ApplicationController
   def create
     upc_code = barcode_params[:upc].match(/0*(\w+)/)[1]
     fail BarcodeException if upc_code =~ /\D/
-    SearchUPCWraper.new.get_product_for(upc_code)
+    product = Product.find_by(barcode: upc_code)
+    SearchUPCWraper.new.get_product_for(upc_code) if product.nil?
     render nothing: true
   end
 
--- spec/app/controllers/products_controller_spec.rb
+++ spec/app/controllers/products_controller_spec.rb
@@ -3,17 +3,9 @@ require 'rails_helper'
 describe Api::ProductsController, type: :controller do
   describe '#create' do
     let(:search_upc_wrapper) { double(:search_upc_wrapper) }
+
     before do
       allow(SearchUPCWraper).to receive(:new).and_return(search_upc_wrapper)
-    end
-    it 'should resolve the product for the given UPC' do
-      expect(search_upc_wrapper).to receive(:get_product_for).with('87654321')
-      post :create, upc: '87654321'
-    end
-
-    it 'should strip leading zeros' do
-      expect(search_upc_wrapper).to receive(:get_product_for).with('87654321')
-      post :create, upc: '0087654321'
     end
 
     it 'should raise a BarcodeException if the upc contains a word character' do
@@ -22,9 +14,38 @@ describe Api::ProductsController, type: :controller do
       end.to raise_error(BarcodeException)
     end
 
-    it do
-      allow(search_upc_wrapper).to receive(:get_product_for).with('123456789012')
-      should permit(:upc).for(:create, params: { 'upc' => '123456789012' })
+    context 'a product with the given upc exists in the local database' do
+      let(:local_product) { 'a_product' }
+
+      before do
+        allow(Product).to receive(:find_by).with(barcode: '87654321').and_return local_product
+      end
+
+      it 'should check the local product database for the given UPC' do
+        expect(Product).to receive(:find_by).with(barcode: '87654321')
+        post :create, upc: '87654321'
+      end
+    end
+
+    context 'a product with the given upc could not be found in the local database' do
+      before do
+        allow(Product).to receive(:find_by).with(barcode: '87654321').and_return nil
+      end
+
+      it 'should strip leading zeros' do
+        expect(search_upc_wrapper).to receive(:get_product_for).with('87654321')
+        post :create, upc: '0087654321'
+      end
+      it 'should resolve the product for the given UPC' do
+        expect(search_upc_wrapper).to receive(:get_product_for).with('87654321')
+        post :create, upc: '87654321'
+      end
+
+      it do
+        allow(Product).to receive(:find_by).with(barcode: '123456789012').and_return nil
+        allow(search_upc_wrapper).to receive(:get_product_for).with('123456789012')
+        should permit(:upc).for(:create, params: { 'upc' => '123456789012' })
+      end
     end
   end
 end
